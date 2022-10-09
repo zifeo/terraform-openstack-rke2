dd6c82ad9bd0f21e5af398a856b1af300bf93c78
Teo Stocco <teo@zifeo.com>
2022-10-09 16:15:07 -0500

    upgrade terraform

--- versions.tf
+++ versions.tf
@@ -1,21 +1,18 @@
 terraform {
-  required_version = ">= 0.14.0"
+  required_version = ">= 1.3.2"
 
   required_providers {
     random = {
       source  = "hashicorp/random"
-      version = ">= 3.1.0"
+      version = ">= 3.4.3"
     }
     null = {
       source  = "hashicorp/null"
-      version = ">= 3.1.0"
+      version = ">= 3.1.1"
     }
     openstack = {
       source  = "terraform-provider-openstack/openstack"
-      version = ">= 1.47.0"
+      version = ">= 1.48.0"
     }
   }
-
-  experiments = [module_variable_optional_attrs]
 }
-
